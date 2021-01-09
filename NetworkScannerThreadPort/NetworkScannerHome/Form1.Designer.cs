563d4e331892111263cddd649514bbbafb6da84c
puneetdsinghal <75376953+puneetdsinghal@users.noreply.github.com>
2021-01-09 13:32:40 +0530

    Add files via upload

---
+++ NetworkScannerThreadPort/NetworkScannerHome/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace NetworkScannerHome
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.maskedTextBoxIP = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBoxSubnetMask = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button_Check_Status = new System.Windows.Forms.Button();
+            this.listResult = new System.Windows.Forms.ListBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnCheckPort = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listPort = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // maskedTextBoxIP
+            // 
+            this.maskedTextBoxIP.Location = new System.Drawing.Point(123, 53);
+            this.maskedTextBoxIP.Mask = "000.000.000.000";
+            this.maskedTextBoxIP.Name = "maskedTextBoxIP";
+            this.maskedTextBoxIP.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBoxIP.TabIndex = 0;
+            this.maskedTextBoxIP.Text = "010011020188";
+            this.maskedTextBoxIP.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBoxIP_MaskInputRejected);
+            this.maskedTextBoxIP.Enter += new System.EventHandler(this.maskedTextBoxIP_Enter);
+            this.maskedTextBoxIP.Leave += new System.EventHandler(this.maskedTextBoxIP_Leave);
+            // 
+            // maskedTextBoxSubnetMask
+            // 
+            this.maskedTextBoxSubnetMask.Location = new System.Drawing.Point(331, 53);
+            this.maskedTextBoxSubnetMask.Mask = "000.000.000.000";
+            this.maskedTextBoxSubnetMask.Name = "maskedTextBoxSubnetMask";
+            this.maskedTextBoxSubnetMask.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBoxSubnetMask.TabIndex = 1;
+            this.maskedTextBoxSubnetMask.Text = "255255255000";
+            this.maskedTextBoxSubnetMask.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBoxSubnetMask_MaskInputRejected);
+            this.maskedTextBoxSubnetMask.Enter += new System.EventHandler(this.maskedTextBoxSubnetMask_Enter);
+            this.maskedTextBoxSubnetMask.Leave += new System.EventHandler(this.maskedTextBoxSubnetMask_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IP Address";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(244, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Subnet Mask";
+            // 
+            // button_Check_Status
+            // 
+            this.button_Check_Status.Location = new System.Drawing.Point(203, 131);
+            this.button_Check_Status.Name = "button_Check_Status";
+            this.button_Check_Status.Size = new System.Drawing.Size(111, 23);
+            this.button_Check_Status.TabIndex = 4;
+            this.button_Check_Status.Text = "Check Status";
+            this.button_Check_Status.UseVisualStyleBackColor = true;
+            this.button_Check_Status.Click += new System.EventHandler(this.button_Check_Status_Click);
+            // 
+            // listResult
+            // 
+            this.listResult.FormattingEnabled = true;
+            this.listResult.Location = new System.Drawing.Point(34, 220);
+            this.listResult.Name = "listResult";
+            this.listResult.Size = new System.Drawing.Size(189, 95);
+            this.listResult.TabIndex = 5;
+            this.listResult.SelectedIndexChanged += new System.EventHandler(this.listResult_SelectedIndexChanged);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(261, 250);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(35, 13);
+            this.lblResult.TabIndex = 6;
+            this.lblResult.Text = "label3";
+            // 
+            // btnCheckPort
+            // 
+            this.btnCheckPort.Location = new System.Drawing.Point(344, 245);
+            this.btnCheckPort.Name = "btnCheckPort";
+            this.btnCheckPort.Size = new System.Drawing.Size(75, 23);
+            this.btnCheckPort.TabIndex = 7;
+            this.btnCheckPort.Text = "Check Port";
+            this.btnCheckPort.UseVisualStyleBackColor = true;
+            this.btnCheckPort.Click += new System.EventHandler(this.btnCheckPort_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(30, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(175, 19);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Select IP to know port";
+            // 
+            // listPort
+            // 
+            this.listPort.FormattingEnabled = true;
+            this.listPort.Location = new System.Drawing.Point(462, 220);
+            this.listPort.Name = "listPort";
+            this.listPort.Size = new System.Drawing.Size(165, 56);
+            this.listPort.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(457, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 19);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Active ports";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listPort);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnCheckPort);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.listResult);
+            this.Controls.Add(this.button_Check_Status);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.maskedTextBoxSubnetMask);
+            this.Controls.Add(this.maskedTextBoxIP);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxIP;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxSubnetMask;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button_Check_Status;
+        private System.Windows.Forms.ListBox listResult;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnCheckPort;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listPort;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ NetworkScannerThreadPort/NetworkScannerHome/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Net;
+using System.Threading;
+
+
+namespace NetworkScannerHome
+{
+    public partial class Form1 : Form
+    {
+        string[] maskedIp;
+        string[] maskedSubnet;
+        int s1=0, s2=0, s3=0, s4=0;
+        int[] maskedIpDigit = new int[4] { 010,011,020,188};
+        int[] maskedSubnetDigit = new int[4] { 255,255,255,000};
+        Ping p = new Ping();
+        PingReply preply;
+        String ipAdd;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Check_Status_Click(object sender, EventArgs e)
+        {
+            
+           
+            //ipAdd = maskedTextBoxIP.Text;
+            // class c subnet mask
+            
+
+            ThreadStart threadCall = delegate {
+                callIpMask(maskedIpDigit, maskedSubnetDigit);
+            };
+            Thread threadObj = new Thread(threadCall);
+            threadObj.Start();
+        }
+
+        private void callIpMask(int[] maskedIpDigit, int[] maskedSubnetDigit)
+        {
+            try
+            {
+                if (maskedSubnetDigit[0] == 255 && maskedSubnetDigit[1] == 255
+                && maskedSubnetDigit[2] == 255
+                && maskedSubnetDigit[3] == 0)
+                {
+                    for (int i = 187; i <= 195; i++)
+                    {
+
+                        ipAdd = maskedIpDigit[0].ToString() + "." +
+                            maskedIpDigit[1].ToString() + "." +
+                            maskedIpDigit[2].ToString() + "." +
+                            i.ToString();
+
+
+
+                        preply = p.Send(ipAdd);
+
+                        if (preply.Status == IPStatus.Success)
+
+                        {
+                            this.Invoke((MethodInvoker)delegate ()
+                            {
+                                listResult.Items.Add(ipAdd + "-[UP]");
+                            });
+
+                        }
+                        else
+                        {
+                            this.Invoke((MethodInvoker)delegate ()
+                            {
+                                listResult.Items.Add(ipAdd.ToString() + "-[Down]");
+                            });
+
+                        }
+
+
+
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("range of ip address is out ");
+            }
+            
+        }
+
+        private void maskedTextBoxIP_Leave(object sender, EventArgs e)
+        {
+
+            string ipaddress = maskedTextBoxIP.Text;
+            maskedIp = ipaddress.Split('.');
+            
+          
+                
+                maskedIpDigit[0] = Convert.ToInt32(maskedIp[0]);
+                maskedIpDigit[1] = Convert.ToInt32(maskedIp[1]);
+                maskedIpDigit[2] = Convert.ToInt32(maskedIp[2]);
+                maskedIpDigit[3] = Convert.ToInt32(maskedIp[3]);
+         
+
+                for (int i = 0; i < maskedIp.Length; i++)
+                {
+                    if ((maskedIpDigit[i] < 0) || (maskedIpDigit[i] > 255))
+                    {
+                        maskedTextBoxIP.Text = "000000000000";
+                        maskedTextBoxIP.ForeColor = Color.Red;
+
+                        MessageBox.Show("enter valid ip");
+                    }
+                    else
+                    {
+                        maskedTextBoxIP.ForeColor = Color.Black;
+                    }
+                }
+            
+         
+            
+        }
+
+        private void maskedTextBoxIP_Enter(object sender, EventArgs e)
+        {
+            maskedTextBoxIP.ForeColor = Color.Black;
+        }
+
+        private void maskedTextBoxIP_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void listResult_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedIpAddress = listResult.SelectedItem.ToString();
+            string[] selectedIpOnly = selectedIpAddress.Split('-');
+
+            lblResult.Text = selectedIpOnly[0];
+        }
+
+        private void btnCheckPort_Click(object sender, EventArgs e)
+        {
+
+            IPGlobalProperties ipProperties = IPGlobalProperties.GetIPGlobalProperties();
+
+            IPEndPoint[] endPoints = ipProperties.GetActiveTcpListeners();
+            TcpConnectionInformation[] tcpConnections = ipProperties.GetActiveTcpConnections();
+
+
+
+            string selectedIP = lblResult.Text.ToString();
+            listPort.Items.Clear();
+
+            foreach (TcpConnectionInformation item in tcpConnections)
+            {
+
+                
+               if (selectedIP == item.LocalEndPoint.Address.ToString())
+                {
+                    listPort.Items.Add(item.LocalEndPoint.Address.ToString() + ":" 
+                        + item.LocalEndPoint.Port.ToString());
+               }
+                if (selectedIP == item.RemoteEndPoint.Address.ToString())
+                {
+                    listPort.Items.Add(item.RemoteEndPoint.Address.ToString() + ":"
+                        + item.RemoteEndPoint.Port.ToString());
+                }
+
+
+            }
+          /*
+            foreach (TcpConnectionInformation info in tcpConnections)
+            {
+                Console.WriteLine("Local : " + info.LocalEndPoint.Address.ToString()
+                + ":" + info.LocalEndPoint.Port.ToString()
+
+                + "\nRemote : " + info.RemoteEndPoint.Address.ToString()
+                + ":" + info.RemoteEndPoint.Port.ToString()
+
+                + "\nState : " + info.State.ToString() + "\n\n");
+            }
+          */
+        }
+
+        private void maskedTextBoxSubnetMask_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void maskedTextBoxSubnetMask_Leave(object sender, EventArgs e)
+        {
+            maskedSubnet = maskedTextBoxSubnetMask .Text.Split('.');
+            
+
+            for (int i = 0; i < maskedSubnet.Length; i++)
+            {
+                
+            }
+            int a = maskedSubnetDigit[0] = Convert.ToInt32(maskedSubnet[0]);
+            int b = maskedSubnetDigit[1] = Convert.ToInt32(maskedSubnet[1]);
+            int c = maskedSubnetDigit[2] = Convert.ToInt32(maskedSubnet[2]);
+            int d = maskedSubnetDigit[3] = Convert.ToInt32(maskedSubnet[3]);
+
+            for (int i = 0; i < maskedSubnet.Length; i++)
+            {
+                if ( (maskedSubnetDigit[i] != 255) && (maskedSubnetDigit[i] != 0))
+                {
+                    maskedTextBoxSubnetMask.Text = "000000000000";
+                    maskedTextBoxSubnetMask.ForeColor = Color.Red;
+
+                    MessageBox.Show("enter valid Subnet Mask");
+                }
+                else
+                {
+                    maskedTextBoxSubnetMask.ForeColor = Color.Black;
+                }
+            }
+        }
+
+        private void maskedTextBoxSubnetMask_Enter(object sender, EventArgs e)
+        {
+            maskedTextBoxSubnetMask.ForeColor = Color.Black;
+        }
+    }
+}
